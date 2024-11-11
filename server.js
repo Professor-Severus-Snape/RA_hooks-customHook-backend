2fba0659763aa4cde6f0576803e72eb16e49ed3d
Julia Fokanova <oncologmma@mail.ru>
2024-11-11 15:48:03 +0300

    add comment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
+// создание объекта приложения Express:
 const app = express();
 
 // безопасность в браузере (несовпадение origin):
